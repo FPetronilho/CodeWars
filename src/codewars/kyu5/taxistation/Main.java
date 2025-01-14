864de5eb4ef15d0fd254cab4b9f002e877b1fcef
FPetronilho <jfgp_10@live.com.pt>
2025-01-14 23:50:36 +0100

    Fixed some issues in the logic of the code and the exercise now passes all fixed tests. However, it only passes in around 85% of the tests with randomly generated inputs.

--- src/codewars/kyu5/taxistation/Main.java
+++ src/codewars/kyu5/taxistation/Main.java
@@ -3,37 +3,48 @@ package codewars.kyu5.taxistation;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(calculateTrips(
-                new int[]{ 100, 50, 80, 40, 40 },
-                new int[]{ 30, 20, 10 }
+                new int[]{ 100, 50, 80 },
+                new int[]{  }
         )));
     }
 
     public static int[] calculateTrips(int[] distances, int[] speeds) {
-        if (speeds == null || speeds.length == 0 || distances == null || distances.length == 0) {
+        // Check for null or empty arrays.
+        if (distances == null || distances.length == 0) {
             return new int[] { 0 };
         }
 
+        if (speeds == null || speeds.length == 0) {
+            return new int[0];
+        }
+
+        // Create Map for different taxis speeds
         Map<Integer, Taxi> taxiMap = new HashMap<>();
         for (int i=0; i<speeds.length; i++) {
             Taxi taxi = new Taxi(speeds[i]);
             taxiMap.put(i, taxi);
         }
 
+
+        // Start looping through the trips
         int currentTravelCounter = 0;
         int numberOfTravels = distances.length;
 
         while (currentTravelCounter<numberOfTravels) {
             Taxi taxiToGo = prioritizeTaxiToGo(taxiMap);
             taxiToGo.setTrips(taxiToGo.getTrips()+1);
-            taxiToGo.setTimeUntilReady(distances[currentTravelCounter]*2);
+            float timeUntilReady = (float) distances[currentTravelCounter] / taxiToGo.getSpeed() * 2;
+            taxiToGo.setTimeUntilReady(timeUntilReady);
             currentTravelCounter++;
         }
 
+        // Create array to return the number of trips in the same order as the speed array parameter
         int[] trips = new int[speeds.length];
         for (int i=0; i<trips.length; i++) {
             trips[i] = taxiMap.get(i).getTrips();
@@ -46,13 +57,12 @@ public class Main {
         Taxi taxiToGo = null;
         for (Map.Entry<Integer, Taxi> entry: taxiMap.entrySet()) {
             Taxi currentTaxi = entry.getValue();
-            if (entry.getKey() == 0) {
-                continue;
-            }
 
             if (taxiToGo == null || currentTaxi.getTimeUntilReady() < taxiToGo.getTimeUntilReady()) {
                 taxiToGo = currentTaxi;
-            } else if (taxiToGo.getTimeUntilReady() == currentTaxi.getTimeUntilReady()) {
+            }
+
+            if (taxiToGo.getTimeUntilReady() == currentTaxi.getTimeUntilReady()) {
                 if (currentTaxi.getSpeed() > taxiToGo.getSpeed()) {
                     taxiToGo = currentTaxi;
                 }
@@ -61,16 +71,11 @@ public class Main {
 
         for (Map.Entry<Integer, Taxi> entry: taxiMap.entrySet()) {
             Taxi currentTaxi = entry.getValue();
-            if (currentTaxi == taxiToGo) {
+            if (Objects.equals(currentTaxi, taxiToGo)) {
                 continue;
             }
 
-            float newTimeUntilReady =
-                    taxiToGo != null ? (currentTaxi.getTimeUntilReady() - taxiToGo.getTimeUntilReady()) : 0f;
-
-            if (entry.getKey() == 0) {
-                continue;
-            }
+            float newTimeUntilReady = currentTaxi.getTimeUntilReady() - taxiToGo.getTimeUntilReady();
 
             currentTaxi.setTimeUntilReady(newTimeUntilReady);
         }
