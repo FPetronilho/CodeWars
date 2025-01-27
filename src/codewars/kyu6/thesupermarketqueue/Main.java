e7be45a529794a1a0e79c07fe5ef18178f2910ae
FPetronilho <jfgp_10@live.com.pt>
2025-01-28 00:06:44 +0100

    Added an optimized solution with Queues to the previous exercise and fixed one issue on the original solution.

--- src/codewars/kyu6/thesupermarketqueue/Main.java
+++ src/codewars/kyu6/thesupermarketqueue/Main.java
@@ -11,6 +11,11 @@ public class Main {
                 2
         ));
 
+        System.out.println(solveSuperMarketQueueOptimized(
+                new int[] {2, 2, 3, 3, 4, 4},
+                2
+        ));
+
     }
 
     public static int solveSuperMarketQueue(int[] customers, int n) {
@@ -20,12 +25,26 @@ public class Main {
 
         List<Integer> tills = new ArrayList<>();
         for (int i=0; i<n; i++) {
-            tills.add(customers[i]);
+            tills.add(i < customers.length ? customers[i] : 0);
         }
 
         for (int i=n; i<customers.length; i++) {
             int minIndex = tills.indexOf(Collections.min(tills));
             tills.set(minIndex, tills.get(minIndex)+customers[i]);
+        }
+
+        return Collections.max(tills);
+    }
+
+    public static int solveSuperMarketQueueOptimized(int[] customers, int n) {
+        PriorityQueue<Integer> tills = new PriorityQueue<>();
+        for (int i=0; i<n; i++) {
+            tills.add(0);
+        }
+
+        for (int customer : customers) {
+            int currentTime = tills.poll();
+            tills.add(currentTime+customer);
         }
 
         return Collections.max(tills);
