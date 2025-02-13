b789d3ef07d12dd7d43c16b554fd0a867637aeec
FPetronilho <jfgp_10@live.com.pt>
2025-02-13 22:49:59 +0100

    Fixed the code for the previous exercise as it was not behaving the way it should after further tests.

--- src/codewars/kyu5/reducedirections/Main.java
+++ src/codewars/kyu5/reducedirections/Main.java
@@ -1,8 +1,7 @@
 package codewars.kyu5.reducedirections;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Stack;
 
 public class Main {
 
@@ -11,85 +10,25 @@ public class Main {
         System.out.println(Arrays.toString(dirReduc(
                 new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}
         )));
-
-        System.out.println(Arrays.toString(dirReducOptimized(
-                new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}
-        )));
     }
 
     public static String[] dirReduc(String[] arr) {
-        int x = 0;
-        int y = 0;
+        Stack<String> stack = new Stack<>();
         for (String direction : arr) {
-            switch (direction) {
-                case "NORTH" -> x += 1;
-                case "SOUTH" -> x -= 1;
-                case "EAST" -> y += 1;
-                case "WEST" -> y -= 1;
+            if (!stack.isEmpty() && isOpposite(stack.peek(), direction)) {
+                stack.pop();
+            } else {
+                stack.push(direction);
             }
         }
 
-        int arrayLength = Math.abs(x) + Math.abs(y);
-        String[] result = new String[arrayLength];
-        for (int i=0; i<Math.abs(x); i++) {
-            if (x < 0) {
-                result[i] = "SOUTH";
-            }
-
-            if (x > 0) {
-                result[i] = "NORTH";
-            }
-        }
-
-        for (int i = Math.abs(x); i<arrayLength; i++) {
-            if (y < 0) {
-                result[i] = "WEST";
-            }
-
-            if (y > 0) {
-                result[i] = "EAST";
-            }
-        }
-
-        return result;
+        return stack.toArray(new String[0]);
     }
 
-
-    public static String[] dirReducOptimized(String[] arr) {
-        List<String> directions = new ArrayList<>();
-        for (String direction : arr) {
-            switch (direction) {
-                case "NORTH" -> {
-                    if (directions.contains("SOUTH")) {
-                        directions.remove("SOUTH");
-                    } else {
-                        directions.add("NORTH");
-                    }
-                }
-                case "SOUTH" -> {
-                    if (directions.contains("NORTH")) {
-                        directions.remove("NORTH");
-                    } else {
-                        directions.add("SOUTH");
-                    }
-                }
-                case "EAST" -> {
-                    if (directions.contains("WEST")) {
-                        directions.remove("WEST");
-                    } else {
-                        directions.add("EAST");
-                    }
-                }
-                case "WEST" -> {
-                    if (directions.contains("EAST")) {
-                        directions.remove("EAST");
-                    } else {
-                        directions.add("WEST");
-                    }
-                }
-            }
-        }
-
-        return directions.toArray(new String[0]);
+    private static boolean isOpposite(String dir1, String dir2) {
+        return (dir1.equals("NORTH") && dir2.equals("SOUTH")) ||
+                (dir1.equals("SOUTH") && dir2.equals("NORTH")) ||
+                (dir1.equals("EAST") && dir2.equals("WEST")) ||
+                (dir1.equals("WEST") && dir2.equals("EAST"));
     }
 }
